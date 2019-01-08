541107216df214575f5749154683cc5abfe5b806
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-08 14:56:24 -0200

    Configuração e Instalação

---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const ModeDev = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: ModeDev ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
+            
+        ]
+    },
+    devServer: {
+        contentBase: './public',
+        port: 9000
+    }
+}
