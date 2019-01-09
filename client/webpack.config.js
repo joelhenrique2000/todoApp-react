095fe239213d021c59108ecfcb8572906597aa7a
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-09 00:41:43 -0200

    corrigindo bugs no webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,66 +1,58 @@
-const ModeDev = process.env.NODE_ENV !== 'production';
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+import path from "path";
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
+import HtmlWebpackPlugin from "html-webpack-plugin";
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: ModeDev ? 'development' : 'production',
-    entry: './src/index.jsx',
-    output: {
-        path: __dirname + '/public',
-        filename: './app.js'
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'app.css'
-        })
-    ],
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
-            }),
-            new OptimizeCSSAssetsPlugin({})
+  entry: path.join(__dirname,'src','index.js'),
+  output: {
+    path: path.join(__dirname,'build'),
+    filename: 'index.bundle.js'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname,'src'),'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'src')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'src','index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'app.css'
+  })
+  ],
+  optimization: {
+      minimizer: [
+          new UglifyJsPlugin({
+              cache: true,
+              parallel: true
+          }),
+          new OptimizeCSSAssetsPlugin({})
+      ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
         ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015', 'react'],
-                        plugins: ['transform-object-rest-spread']
-                    }
-                }
-            },
-            {
-                test: /\.s?[ac]ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ]
-            }, {
-                test: /\.(png|svg|jpg|gif|eot|ttf|woff2|woff)$/,
-                use: ['file-loader']
-            }
-            
-        ]
-    },
-    resolve: {
-        extensions: ['js','jsx'],
-        alias: {
-            modules: __dirname + '/node_modules'
-        },
-    },
-    devServer: {
-        contentBase: './public',
-        port: 9000
-    }
-}
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ["file-loader"]
+      }
+    ]
+  }
+};
