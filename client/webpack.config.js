506e0cc472359fbc373dcb8230266276d77d0964
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-11 15:17:25 -0200

    voltando para o webpack 1

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,58 +1,40 @@
-import path from "path";
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-import HtmlWebpackPlugin from "html-webpack-plugin";
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname,'src','index.js'),
-  output: {
-    path: path.join(__dirname,'build'),
-    filename: 'index.bundle.js'
-  },
-  mode: process.env.NODE_ENV || 'development',
-  resolve: {
-    modules: [path.resolve(__dirname,'src'),'node_modules']
-  },
-  devServer: {
-    contentBase: path.join(__dirname,'src')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname,'src','index.html')
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'app.css'
-  })
-  ],
-  optimization: {
-      minimizer: [
-          new UglifyJsPlugin({
-              cache: true,
-              parallel: true
-          }),
-          new OptimizeCSSAssetsPlugin({})
-      ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        loaders: ["file-loader"]
-      }
-    ]
-  }
-};
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [ 
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
+    }
+}
