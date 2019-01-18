b37a3b0868096ba77b00df7101f11d5ce11b5b85
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-18 19:04:19 -0200

    Exclusão de TODOs

--- client-com-redux/src/reducers/todoReducer.js
+++ client-com-redux/src/reducers/todoReducer.js
@@ -3,7 +3,7 @@ import actionType from '../constants/actionType'
 const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
         case 'TODO_SEARCHED':
