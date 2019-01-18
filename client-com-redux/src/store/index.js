0232a6c83fc7d70bfe171fb9f1488794788947ab
joelhenrique2000 <joelhenrique2000@gmail.com>
2019-01-18 00:42:43 -0200

    Aplicando Middleware (Redux-promise)

--- client-com-redux/src/store/index.js
+++ client-com-redux/src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
+import promise from 'redux-promise'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
     && window.__REDUX_DEVTOOLS_EXTENSION__()
-export const Store = createStore(reducers, devTools)
+export const Store = applyMiddleware(promise)(createStore)(reducers, devTools)
